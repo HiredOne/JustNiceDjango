5b6a98e603dd9ff265547bf02ea30a8beb274aaa
HiredOne <anthonycheang23@gmail.com>
2021-05-29 20:28:40 +0800

    Modified views.py in leads to include a UserListView. Added template for view above

--- JustNice/leads/views.py
+++ JustNice/leads/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from .models import Lead
 from django.contrib.auth.models import User
+from django.core.serializers import serialize
 from .serializers import LeadSerializer, CurrentUserSerializer
 from rest_framework.generics import ListCreateAPIView
 from django.views.generic import (
-        CreateView,
-        DetailView,
         ListView,
-        UpdateView,
-        ListView,
-        DeleteView
 )
 # Create your views here.
 
@@ -17,7 +13,18 @@ class LeadListCreate(ListCreateAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
 
-# Trying to create a User version of the view
+# Trying to create a User version of the view2
 class UserListCreate(ListView):
     queryset = User.objects.all()
+    template_name = 'users/user_list.html'
     serializer_class = CurrentUserSerializer
+    data = serialize("json", User.objects.all())
+
+    # What to (forcefully) render on a GET req
+    def get(self, request, *args, **kwargs):
+        context = {'data': self.data, "dic" : self.queryset.values()}
+        return render(request, self.template_name, context)
+
+    # Commented out cause not impl yet
+    # def post(self, request, *args, **kwargs):
+    #     context = {<fill in stuff>}
