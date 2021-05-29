65e33bae463a2001cd0fb99a71f40fe2cb5cb298
HiredOne <anthonycheang23@gmail.com>
2021-05-30 01:01:07 +0800

    Updated CORS information at the bottom

--- JustNice/live1/settings.py
+++ JustNice/live1/settings.py
@@ -142,6 +142,7 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home' 
 
 # For fixing CORS bug
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True   
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
